d1d75e5cc64f5421af34fa9e2922c10ec6f4a911
Opilane TTHK <opilane@tthk.ee>
2026-01-22 10:04:40 +0200

    file ja meetod

--- Meetodid/Meetodid1/Program.cs
+++ Meetodid/Meetodid1/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Meetodid1
+﻿using System.IO.MemoryMappedFiles;
+
+namespace Meetodid1
 {
     internal class Program
     {
@@ -12,30 +14,65 @@
             return sisend;
         }
 
-        
 
 
 
-        static void Main(string[] args)
-        
+
+        private static void Main(string[] args)
         {
 
             List<string> filmid = GetMovies();
             Console.WriteLine("Milline on sinu lemmikfilm");
             string lemmikfilm = ReadAnswer();
             DoesMovieExist(filmid, lemmikfilm);
+
+
+            List<string> otsitavadFilmid = new List<string> { "Terminator", "Vanamehe film", "Kratt" };
+            IdentifyMovies(filmid, otsitavadFilmid);
         }
-        
+
+
+        public static void IdentifyMovies(List<string> collection, List<string> filter)
+        {
+            string messages = "";
+            foreach (var movie in collection)
+            {
+                int itemnr = 0;
+                foreach (var filterItem in filter)
+                {
+                    if (itemnr == 0)
+                    {
+                        messages += "Ill be back. \n";
+                    }
+                    else if (itemnr == 1)
+                    {
+                        messages += "Šnikurs\n";
+                    }
+                    else if (itemnr == 2)
+                    {
+                        messages += "Vaata et ta sul tehisplära ajama ei hakka\n";
+                    }
+                    itemnr++;
+                }
+                Console.WriteLine(messages);
+            }
+        }
+
+
+
+
+
+
+
         public static List<string> DoYouLikeThiMovie(List<string> collection, string movieToAdd)
         {
 
-        
-            if (Collection.Contains(movieToAdd) == false)
+
+            if (collection.Contains(movieToAdd) == false)
             {
                 Console.WriteLine($"Kas sulle meeldi {movieToAdd}?");
                 string vastus = ReadAnswer();
-            }
-            if (vastus == "jah")
+                if (vastus == "jah")
             {
                 collection.Add(movieToAdd);
                 Console.WriteLine("Lisasin filmi sulle");
@@ -44,13 +81,15 @@
             {
                 Console.WriteLine("Aga miks? See on ju hea film?");
             }
+            }
+            
 
-                return collection;
+            return collection;
         }
         public static void DoesMovieExist(List<string> collection, string findThis)
         {
             bool itExists = false;
-            foreach  (var item in collection)
+            foreach (var item in collection)
             {
                 if (item == findThis)
                 {
@@ -93,4 +132,4 @@
         }
 
     }
-}   
+}
---
+++ Meetodid/failid/File/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Kirjuta siia oma postikasti aadress");
+
+
+        //string filePath = "C:\\Users\\opilane\\Source\\Repos\\harjutused Henri Hunt\\Meetodid\\failid\\File\\postkastiaadress.txt";
+        //string aadress = Console.ReadLine();
+
+        //File.WriteAllText(filePath, aadress);
+
+        Console.WriteLine("Kõik laulusõnad");
+
+        DisplayThisFile();
+    }
+    private static void DisplayThisFile()
+    {
+        string line = "";
+        try
+        {
+            using (
+                StreamReader readThisThing = new StreamReader
+            ("C:\\Users\\opilane\\Source\\Repos\\harjutused Henri Hunt\\Meetodid\\failid\\File\\jjjj.txt") )
+            {
+                int LineCount = 0;
+                while (readThisThing.EndOfStream == false)
+                {
+                    LineCount++;
+                    line = readThisThing.ReadLine();
+                    int pikkus = line.Length;
+                    Console.WriteLine(LineCount + " " + line + " . Reapikkus on" + pikkus);
+                }
+                readThisThing.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Ei saa lugeda sest:" + e.Message);
+            throw;
+        }
+    }
+
+}
